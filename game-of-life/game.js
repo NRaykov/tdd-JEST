06438883ec9bc4d04f3b319bb4d50f3bdc830022
NRaykov <nediotenev@gmail.com>
2020-04-07 13:48:47 +0300

    Code refactor (using ternary operator)

--- game-of-life/game.js
+++ game-of-life/game.js
@@ -1,17 +1,4 @@
-const isAlive = (cell, neighbours) => {
-  // Case -> !Boolean(cell) -> dead cell (cell = false or cell = 0)
-  if(!Boolean(cell) && neighbours === 3) {
-    return 1;
-  }
-
-  // Case -> Boolean(cell) -> live cell (cell = true or cell = 1)
-  if(Boolean(cell) && neighbours === 2) {
-    return 1;
-  }
-
-  //Default cell's state => false
-  return 0
-}
+const isAlive = (cell, neighbours) => (neighbours === 3 || (Boolean(cell) && neighbours === 2)) ? 1 : 0;
 
 window.game = {
   isAlive
